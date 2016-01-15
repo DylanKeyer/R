322c8d313cc3532203879af721030d05cd581c5f
Dylan <keyer42@gmail.com>
2016-01-15 02:41:58 -0500

    Update binarize.R

--- binarize.R
+++ binarize.R
@@ -1,3 +1,8 @@
+#It's important to note that the observed levels of the training and testing data might be different. This function takes that into
+#account, but it is necessary that the (potential) levels of the training and testing data are set to be the same.
+#for example:
+#potential_levels <- unique(c(levels(train),levels(test)))
+
 binarize <- function(labels,data){
   if(!(class(data) %in% c('tbl_df','tbl','data.frame'))) print('Error: Data must be in the form of a data frame.')
   else{
